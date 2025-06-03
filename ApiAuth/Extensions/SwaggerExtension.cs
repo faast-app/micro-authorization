270579dda2341c86381d165d44846089b64b940f
guido_matos_faast <guido.matos@faast.cl>
2025-06-02 20:07:31 -0500

    v1.4.0

--- ApiAuth/Extensions/SwaggerExtension.cs
+++ ApiAuth/Extensions/SwaggerExtension.cs
@@ -16,7 +16,7 @@ public static class SwaggerExtension
             c.IncludeXmlComments(xmlPath);
             c.SwaggerDoc("v1", new OpenApiInfo
             {
-                Version = "v1.3.0",
+                Version = "v1.4.0",
                 Title = "API Auth",
                 Description = "REST API",
                 Contact = new OpenApiContact()
